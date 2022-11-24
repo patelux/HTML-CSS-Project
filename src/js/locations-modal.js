29c58fe10701b0b8d4d6653143c46b7ee358acce
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-11-24 14:37:06 +0200

    added modal-locations

--- src/js/locations-modal.js
+++ src/js/locations-modal.js
@@ -4,10 +4,8 @@
     closeModalBtn: document.querySelector('[data-locations-close]'),
     modal: document.querySelector('[data-locations]'),
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }

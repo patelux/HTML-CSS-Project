8d3c992f25302d2b4b1c40e909284129fa57e1b2
patelux <116380879+patelux@users.noreply.github.com>
2022-11-24 18:38:40 +0000

    Deploying to gh-pages from @ patelux/HTML-CSS-Project@26edd057e8f57f867fb3313ded1788e3d000342f 🚀

--- index.5bfc5247.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body"),menuList:document.querySelector(".top__list")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.5bfc5247.js.map

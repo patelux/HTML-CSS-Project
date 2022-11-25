a119e08200f507b8cc9f8ba19e6186b1a979d015
iTommma <116196545+iTommma@users.noreply.github.com>
2022-11-25 07:11:46 +0000

    Deploying to gh-pages from @ patelux/HTML-CSS-Project@6e4eee9fe435bbea88c07cdffd9eda4fad5699e8 🚀

---
+++ index.7da24a36.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".gallery__swiper",{autoplay:{delay:3e3,disableOnInteraction:!1,pauseOnMouseEnter:!0},loop:!0});swiper=new Swiper(".review__swiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.7da24a36.js.map

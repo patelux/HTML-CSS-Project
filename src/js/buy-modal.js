8995a93f01c2e8fc0c709db24064fdfba9a13f6e
Anna Atiskova <anna.atiskova@gmail.com>
2022-11-26 03:19:23 +0200

    modal

--- src/js/buy-modal.js
+++ src/js/buy-modal.js
@@ -3,6 +3,7 @@
       openModalBtn: document.querySelector('[data-buy-open]'),
       closeModalBtn: document.querySelector('[data-buy-close]'),
       modal: document.querySelector('[data-buy]'),
+      body: document.querySelector('body'),
     };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+      refs.body.classList.toggle('no-scroll');
     }
   })();

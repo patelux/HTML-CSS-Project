1764d43213951fd560e6f0d57f05db248880b721
Anna Atiskova <anna.atiskova@gmail.com>
2022-11-27 11:02:01 +0200

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './js/mobile-menu';
 import './js/anchor-scroll';
 import './js/buy-modal';
+import './js/franchise';
 import './js/gallery-swiper';
 import './js/locations-modal';
+import './js/read-more';
 import './js/products-modal';

8f4bfd278a92cc3b49e3ec2d7a6cf1b203faa99d
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2022-11-27 12:58:05 +0200

    finished

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-readmore]'),
+    openModalBtn: document.querySelector('[data-readmore-open]'),
     closeModalBtn: document.querySelector('[data-readmore-close]'),
-    modal: document.querySelector('[data-readmore-open]'),
+    modal: document.querySelector('[data-readmore]'),
   };
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
